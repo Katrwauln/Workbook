c1fa388561ec551f648bf6a97eb58b4169996935
Katrwauln <46534089+Katrwauln@users.noreply.github.com>
2020-08-17 18:36:31 -0500

    Activity 1

---
+++ hello.py
@@ -0,0 +1,17 @@
+# This program says hello and asks for my name.
+
+print('Hello world!')
+print('What is your name?') # asks for name
+
+myName = input()
+
+print('It is nice to meet you, ' + myName)
+
+print('The length of your name is: ')
+print(len(myName))
+
+print('What is your age?') # ask for age
+
+myAge = input()
+
+print('You will be ' + str(int(myAge)+1) + ' in a year.')
